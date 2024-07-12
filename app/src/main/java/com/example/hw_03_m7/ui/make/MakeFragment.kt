9145f31cf29e24ecebfbc80bbb547faf7a172720
Asanaliev Temirlan <tonyadam050607@gmail.com>
2024-07-13 00:31:13 +0600

    add notification

--- app/src/main/java/com/example/hw_03_m7/ui/make/MakeFragment.kt
+++ app/src/main/java/com/example/hw_03_m7/ui/make/MakeFragment.kt
@@ -11,6 +11,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -31,9 +32,7 @@ class MakeFragment : Fragment() {
     private var existingMedicine: MedicinesModel? = null
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         _binding = FragmentMakeBinding.inflate(inflater, container, false)
         return binding.root
@@ -41,42 +40,62 @@ class MakeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val bottomNavView = activity?.findViewById<BottomNavigationView>(R.id.nav_view)
-        bottomNavView?.visibility = View.GONE
+        hideBottomNavigation()
         setupUI()
+        initializeData()
         setUpListeners()
-        initialization()
-        update()
-        checkBoxListener()
     }
 
-    private fun checkBoxListener() = with(binding) {
+    private fun hideBottomNavigation() {
+        activity?.findViewById<BottomNavigationView>(R.id.nav_view)?.visibility = View.GONE
+        activity?.findViewById<Button>(R.id.btn_plus)?.visibility = View.GONE
+    }
+
+    private fun showBottomNavigation() {
+        activity?.findViewById<BottomNavigationView>(R.id.nav_view)?.visibility = View.VISIBLE
+        activity?.findViewById<Button>(R.id.btn_plus)?.visibility = View.VISIBLE
+    }
+
+    private fun setupUI() = with(binding) {
+        selectTimeButton.setOnClickListener { showTimePickerDialog() }
+        selectDateButton.setOnClickListener { showDatePickerDialog() }
+        selectDosageButton.setOnClickListener { showDosagePickerDialog() }
         timeCheckbox.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                dateLayout.visibility = View.VISIBLE
-                delayedMedicineButton.isEnabled = true
-            } else {
-                dateLayout.visibility = View.GONE
-                delayedMedicineButton.isEnabled = false
-                dateTextView.text = null
-            }
+            dateLayout.isVisible = isChecked
+            delayedMedicineButton.isEnabled = isChecked
+            if (!isChecked) dateTextView.text = null
         }
     }
 
-    private fun update() = with(binding) {
+    private fun initializeData() {
         val medicineId = args.medicinesId
         if (medicineId != -1) {
             viewModel.getMedicineId(medicineId).observe(viewLifecycleOwner) { medicine ->
                 medicine?.let {
                     existingMedicine = it
-                    etTitle.setText(it.title)
-                    etDescription.setText(it.description)
-                    dosageTextView.text = it.quantity.toString()
-                    timeTextView.text = it.time
-                    dateTextView.text = it.date
+                    bindMedicineData(it)
                 }
             }
         }
+    }
+
+    private fun bindMedicineData(medicine: MedicinesModel) = with(binding) {
+        etTitle.setText(medicine.title)
+        etDescription.setText(medicine.description)
+        dosageTextView.text = medicine.quantity.toString()
+        timeTextView.text = medicine.time
+        dateTextView.text = medicine.date
+    }
+
+    private fun setUpListeners() = with(binding) {
+        closeButton.setOnClickListener { navigateBack() }
+        addButton.setOnClickListener { handleMedicineAction(isUpdate = false) }
+        changeButton.setOnClickListener { handleMedicineAction(isUpdate = true) }
+        delayedMedicineButton.setOnClickListener {  }
+    }
+
+    private fun navigateBack() {
+        findNavController().navigate(R.id.navigation_medicines)
     }
 
     private fun handleMedicineAction(isUpdate: Boolean) = with(binding) {
@@ -86,135 +105,78 @@ class MakeFragment : Fragment() {
         val time = timeTextView.text.toString()
         val date = dateTextView.text.toString()
 
-        if (medicineName.isEmpty() || dosageText.isEmpty() || time.isEmpty() || date.isEmpty() || description.isEmpty()) {
-            Toast.makeText(requireContext(), "Заполните все поля", Toast.LENGTH_SHORT).show()
+        if (medicineName.isEmpty() || description.isEmpty() || dosageText.isEmpty() || time.isEmpty() || date.isEmpty()) {
+            showToast("Заполните все поля")
             return
         }
 
         val dosage = dosageText.toIntOrNull() ?: 1
         if (dosage == 0) {
-            Toast.makeText(requireContext(), "Некорректная дозировка", Toast.LENGTH_SHORT).show()
+            showToast("Некорректная дозировка")
             return
         }
 
         val medicinesModel = existingMedicine?.copy(
-            title = medicineName,
-            description = description,
-            quantity = dosage,
-            time = time,
-            date = date
+            title = medicineName, description = description, quantity = dosage, time = time, date = date
         ) ?: MedicinesModel(
-            title = medicineName,
-            description = description,
-            quantity = dosage,
-            time = time,
-            date = date
+            title = medicineName, description = description, quantity = dosage, time = time, date = date
         )
 
         if (isUpdate) {
             viewModel.updateMedicine(medicinesModel)
-            Toast.makeText(requireContext(), "Медикамент обновлен", Toast.LENGTH_SHORT).show()
+            showToast("Медикамент обновлен")
         } else {
             viewModel.insertMedicines(medicinesModel)
-            Toast.makeText(requireContext(), "Медикамент добавлен", Toast.LENGTH_SHORT).show()
+            showToast("Медикамент добавлен")
         }
-        findNavController().navigate(R.id.navigation_medicines)
+        navigateBack()
     }
 
-
-    private fun setUpListeners() = with(binding) {
-        closeButton.setOnClickListener {
-            findNavController().navigate(R.id.navigation_medicines)
-        }
-        addButton.setOnClickListener {
-            handleMedicineAction(false)
-        }
-        changeButton.setOnClickListener {
-            handleMedicineAction(true)
-        }
-        delayedMedicineButton.setOnClickListener {
-
-        }
+    private fun showToast(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
     }
 
-    private fun setupUI() = with(binding) {
-        selectTimeButton.setOnClickListener {
-            showTimePickerDialog()
-        }
-        selectDateButton.setOnClickListener {
-            showDatePickerDialog()
-        }
-        selectDosageButton.setOnClickListener {
-            showDosagePickerDialog()
-        }
-        delayedMedicineButton.setOnClickListener {
-
-        }
-    }
-
-    private fun showTimePickerDialog() = with(binding) {
+    private fun showTimePickerDialog() {
         val calendar = Calendar.getInstance()
         val hour = calendar.get(Calendar.HOUR_OF_DAY)
         val minute = calendar.get(Calendar.MINUTE)
 
-        val timePickerDialog = TimePickerDialog(
-            requireContext(),
-            { _, selectedHour, selectedMinute ->
-                timeTextView.text = String.format("%02d:%02d", selectedHour, selectedMinute)
-            }, hour, minute, true
-        )
-        timePickerDialog.show()
+        TimePickerDialog(requireContext(), { _, selectedHour, selectedMinute ->
+            binding.timeTextView.text = String.format("%02d:%02d", selectedHour, selectedMinute)
+        }, hour, minute, true).show()
     }
 
-    private fun showDatePickerDialog() = with(binding) {
+    private fun showDatePickerDialog() {
         val calendar = Calendar.getInstance()
         val year = calendar.get(Calendar.YEAR)
         val month = calendar.get(Calendar.MONTH)
         val day = calendar.get(Calendar.DAY_OF_MONTH)
 
-        val datePickerDialog = DatePickerDialog(
-            requireContext(),
-            { _, selectedYear, selectedMonth, selectedDay ->
-                dateTextView.text =
-                    String.format("%02d/%02d/%04d", selectedDay, selectedMonth + 1, selectedYear)
-            }, year, month, day
-        )
-        datePickerDialog.show()
+        DatePickerDialog(requireContext(), { _, selectedYear, selectedMonth, selectedDay ->
+            binding.dateTextView.text = String.format("%02d/%02d/%04d", selectedDay, selectedMonth + 1, selectedYear)
+        }, year, month, day).show()
     }
 
-    private fun showDosagePickerDialog() = with(binding) {
-        val builder = AlertDialog.Builder(requireContext())
-        builder.setTitle("Dosage")
-
-        val input = EditText(requireContext())
-        input.inputType = InputType.TYPE_CLASS_NUMBER
-        builder.setView(input)
-
-        builder.setPositiveButton("OK") { _, _ ->
-            val dosage = input.text.toString()
-            if (dosage.isNotEmpty()) {
-                dosageTextView.text = dosage
-            } else {
-                Toast.makeText(requireContext(), "Dosage cannot be empty", Toast.LENGTH_SHORT)
-                    .show()
+    private fun showDosagePickerDialog() {
+        AlertDialog.Builder(requireContext()).apply {
+            setTitle("Dosage")
+            val input = EditText(requireContext()).apply { inputType = InputType.TYPE_CLASS_NUMBER }
+            setView(input)
+            setPositiveButton("OK") { _, _ ->
+                val dosage = input.text.toString()
+                if (dosage.isNotEmpty()) {
+                    binding.dosageTextView.text = dosage
+                } else {
+                    showToast("Dosage cannot be empty")
+                }
             }
-        }
-        builder.setNegativeButton("Cancel") { dialog, _ -> dialog.cancel() }
-
-        builder.show()
-    }
-
-    private fun initialization() {
-        val btnPlus = activity?.findViewById<Button>(R.id.btn_plus)
-        btnPlus?.visibility = View.GONE
+            setNegativeButton("Cancel") { dialog, _ -> dialog.cancel() }
+        }.show()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        val bottomNavView = activity?.findViewById<BottomNavigationView>(R.id.nav_view)
-        bottomNavView?.visibility = View.VISIBLE
-        val btnPlus = activity?.findViewById<Button>(R.id.btn_plus)
-        btnPlus?.visibility = View.VISIBLE
+        showBottomNavigation()
         _binding = null
     }
 }
--- app/src/main/java/com/example/hw_03_m7/ui/medicines/MedicinesFragment.kt
+++ app/src/main/java/com/example/hw_03_m7/ui/medicines/MedicinesFragment.kt
@@ -1,9 +1,5 @@
 package com.example.hw_03_m7.ui.medicines
 
-import android.app.AlarmManager
-import android.app.PendingIntent
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -19,100 +15,59 @@ import com.example.hw_03_m7.R
 import com.example.hw_03_m7.data.model.MedicinesModel
 import com.example.hw_03_m7.databinding.FragmentMedicinesBinding
 import com.example.hw_03_m7.ui.interfaces.OnClickItem
-import com.example.hw_03_m7.ui.notification.AlarmReceiver
+import com.example.hw_03_m7.utils.NotificationUtils
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
-import java.text.SimpleDateFormat
-import java.util.Calendar
-import java.util.Locale
 
 @AndroidEntryPoint
 class MedicinesFragment : Fragment(), OnClickItem {
 
     private var _binding: FragmentMedicinesBinding? = null
     private val binding get() = _binding!!
-    private lateinit var adapter: MedicinesAdapter
     private val viewModel: MedicinesViewModel by viewModels()
+    private lateinit var adapter: MedicinesAdapter
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentMedicinesBinding.inflate(inflater, container, false)
-        (activity as? AppCompatActivity)?.setSupportActionBar(binding.toolbar)
-        (activity as? AppCompatActivity)?.supportActionBar?.setDisplayHomeAsUpEnabled(false)
-        (activity as? AppCompatActivity)?.supportActionBar?.setDisplayShowTitleEnabled(false)
+        _binding = FragmentMedicinesBinding.inflate(inflater, container, false).apply {
+            (activity as? AppCompatActivity)?.let {
+                it.setSupportActionBar(toolbar)
+                it.supportActionBar?.setDisplayHomeAsUpEnabled(false)
+                it.supportActionBar?.setDisplayShowTitleEnabled(false)
+            }
+        }
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         adapter = MedicinesAdapter(onClickItem = this, onLongClickItem = this)
-        addData()
         binding.rvMedicines.adapter = adapter
-        initListener()
-    }
 
-    private fun addData() {
         viewModel.getAll().observe(viewLifecycleOwner) { medicines ->
             Log.e("TAG", "MFragment $medicines")
             adapter.submitList(medicines)
-
             medicines.forEach { medicine ->
-                val timeInMillis = convertTimeToMillis(medicine.time)
-                scheduleNotification(requireContext(), medicine.title, "Time to take your medication: ${medicine.title}", timeInMillis)
+                val timeInMillis = NotificationUtils.convertTimeToMillis(medicine.time)
+                NotificationUtils.scheduleNotification(
+                    requireContext(), medicine.title,
+                    "Time to take your medication: ${medicine.title}", timeInMillis
+                )
             }
         }
     }
 
-    private fun initListener() {
-
-    }
-
-    private fun convertTimeToMillis(time: String): Long {
-        val sdf = SimpleDateFormat("HH:mm", Locale.getDefault())
-        val date = sdf.parse(time)
-
-        val calendar = Calendar.getInstance()
-        calendar.time = date
-        calendar.set(Calendar.SECOND, 0)
-        calendar.set(Calendar.MILLISECOND, 0)
-
-        if (calendar.timeInMillis < System.currentTimeMillis()) {
-            calendar.add(Calendar.DAY_OF_YEAR, 1)
-        }
-
-        return calendar.timeInMillis
-    }
-
-    private fun scheduleNotification(context: Context, title: String, message: String, timeInMillis: Long) {
-        val intent = Intent(context, AlarmReceiver::class.java).apply {
-            putExtra("title", title)
-            putExtra("message", message)
-        }
-        val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
-
-        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        alarmManager.setExact(AlarmManager.RTC_WAKEUP, timeInMillis, pendingIntent)
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-
     override fun onLongClick(medicinesModel: MedicinesModel) {
-        val builder = AlertDialog.Builder(requireContext())
-        with(builder) {
+        AlertDialog.Builder(requireContext()).apply {
             setTitle("Delete Medicine")
             setMessage("Are you sure you want to delete this medicine?")
-            setPositiveButton("Yes") { dialog, which ->
+            setPositiveButton("Yes") { _, _ ->
                 lifecycleScope.launch {
                     viewModel.delete(medicinesModel)
                 }
             }
-            setNegativeButton("No") { dialog, which ->
+            setNegativeButton("No") { dialog, _ ->
                 dialog.dismiss()
             }
             show()
@@ -122,5 +77,10 @@ class MedicinesFragment : Fragment(), OnClickItem {
     override fun onClick(medicinesModel: MedicinesModel) {
         val action = MedicinesFragmentDirections.actionNavigationMedicinesToNavigationMake(medicinesModel.id!!)
         findNavController().navigate(action)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
